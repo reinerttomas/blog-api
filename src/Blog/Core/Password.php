5410faae957a458cf7d32d0e952f53d74e60367f
Tomáš Reinert <reinerttomas@gmail.com>
2021-07-14 21:33:47 +0200

    password

---
+++ src/Blog/Core/Password.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Blog\Core;
+
+class Password
+{
+    public static function hash(string $password): string
+    {
+        return password_hash($password, PASSWORD_BCRYPT);
+    }
+
+    public static function verify(string $password, string $hash): bool
+    {
+        return password_verify($password, $hash);
+    }
+}
